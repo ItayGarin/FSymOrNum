4d8d206f89ac7f66c6b2db813548f5879e826507
Itay Garin <thifixp@gmail.com>
2020-08-09 20:57:30 +0300

    Speed-up the freq aggregation with a mut Dictionary

--- FSymOrNum/FreqGetter.fs
+++ FSymOrNum/FreqGetter.fs
@@ -1,8 +1,12 @@
 module FreqGetter
+
 open System.IO
 open FSharpPlus
+open System.Collections.Generic
 
-let private incMapEntry map key =
+// --------------- Immutable getFreq ------------------
+
+let incMapEntry map key =
     let currVal = Map.tryFind key map
     let nextVal =
         match currVal with
@@ -10,23 +14,49 @@ let private incMapEntry map key =
         | None -> 1
     Map.add key nextVal map
 
-let getFreq seq =
-    seq
+let getFreq str =
+    str
     |> Seq.fold incMapEntry Map.empty
+
+// --------------- Mutable getFreq ------------------
+
+let incMapEntryMut (dict: Dictionary<char, int>) (key: char) =
+    let mutable curr = 0
+    if dict.TryGetValue(key, &curr) then
+        dict.[key] <- curr + 1
+    else
+        dict.TryAdd(key, 1) |> ignore
+    dict
+
+let inline toMap kvps =
+    kvps
+    |> Seq.map (|KeyValue|)
+    |> Map.ofSeq
+
+let getFreqMut str =
+    let dict = new Dictionary<char, int>()
+    str
+    |> Seq.fold incMapEntryMut dict
+    |> toMap
+
+// --------------- GetFileFreq(s) ------------------
 
 let getFileFreq path =
     async {
         let! text = File.ReadAllTextAsync path |> Async.AwaitTask
-        return getFreq text
+        return getFreqMut text
     }
 
 let mergeFreqs maps =
     let folder = Map.unionWith (+) 
     Seq.fold folder Map.empty maps
 
-let getFilesFreq paths =
+let getFilesFreqEx filter paths =
     paths
     |> Seq.map getFileFreq
     |> Async.Parallel
     |> Async.RunSynchronously
     |> mergeFreqs
+
+let getFilesFreq paths =
+    getFilesFreqEx id paths
